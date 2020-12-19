680a978e2b59d47a2ee7911fe382270a06a92f48
Cody Miller <cody.miller@learningfuze.com>
2020-12-18 19:39:48 -0800

    Initial Commit Added ability to create project, columns, and cards

---
+++ card-templates.js
@@ -0,0 +1,15 @@
+module.exports = [
+  {
+    content: "## Make sure to add these in reverse order"
+  },
+  {
+    content: `
+      ### Employer can see your Professional GitHub Username
+
+      - [ ] Read about [what makes a GitHub username professional](https://github.com/Learning-Fuze/portfolio_guidelines/tree/master/requirement_details/github_profile/README.md#step-1---a-great-username)
+      - [ ] Verify your GitHub username is professional, changing it if necessary.
+      - [ ] Learn how to [copy a GitHub Projects card link](https://github.com/Learning-Fuze/portfolio_guidelines/tree/master/guides/project_management/get_github_projects_card_link.md).
+      - [ ] Slack this task card's title, this task card's link, and your GitHub profile URL to your Basic Portfolio Reviewer for feedback.
+    `
+  }
+]
---
+++ index.js
@@ -0,0 +1,43 @@
+const { Octokit } = require("@octokit/core");
+const dedent = require("dedent-js");
+const cardTemplates = require("./card-templates.js")
+
+const mediaType = {
+  previews: [
+    'inertia'
+  ]
+}
+
+const octokit = new Octokit({
+  auth: "7187747affd5ee2f02b969f7296623834b894826"
+})
+
+async function buildProject(){
+  const { data: { id: projectId } } = await createProject("Web Development Portfolio")
+  const createdColumns = await createColumns(projectId, ["Pending", "In Progress", "In Review", "Completed"])
+  const pendingColumnId = createdColumns.find(column => column.name === "Pending").id
+  createCards(pendingColumnId, cardTemplates)
+}
+
+function createProject(name){
+  return octokit.request('POST /user/projects', { name, mediaType })
+}
+
+async function createColumns(project_id, columns){
+  const createdColumns = [];
+  for(let i = 0; i < columns.length; i++){
+    const name = columns[i]
+    const column = await octokit.request(`POST /projects/${project_id}/columns`, { project_id, name, mediaType }).catch(console.log)
+    createdColumns.push(column.data)
+  }
+  return createdColumns;
+}
+
+async function createCards(column_id, cards){
+  for(let i = 0; i < cards.length; i++){
+    const note = dedent(cards[i].content)
+    await octokit.request(`POST /projects/columns/${column_id}/cards`, { column_id, note, mediaType }).catch(console.log)
+  }
+}
+
+buildProject()
